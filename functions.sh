198712c49106219b76fd222ec4e7e5e6c1de9fd4
Sri <srinu1289@gmail.com>
2026-03-30 17:06:19 +0530

    functions

--- functions.sh
+++ functions.sh
@@ -8,17 +8,17 @@ fi
 VALIDATE () {
   if [ $1 -ne 0 ]; then
     echo "Failed to install $2."
-#    exit 1
+ exit 1
   else
     echo "$2 installed successfully."
   fi
 }
 
-dnf install ngninx -y
+dnf install nginx -y
 VALIDATE $? "Installing nginx web server"
 
 
-dnf install mysqdl -y
+dnf install mysql -y
 VALIDATE $? "Installing mysql database server"
 
 dnf install nodejs -y
