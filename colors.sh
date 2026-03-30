2067384e39effc1b49e79100f06ed266ffb5d404
Sri <srinu1289@gmail.com>
2026-03-30 22:39:15 +0530

    loops

---
+++ colors.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+R=ed='\033[0;31m'
+G=reen='\033[0;32m'
+Y=ellow='\033[0;33m'
+B=lue='\033[0;34m'
+N=oColor='\033[0m'
+
+echo -e "${R}Hello all${N}, ${G}welcome to the world of shell scripting!${N}"
