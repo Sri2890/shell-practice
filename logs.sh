0a546a82139745d663d4e7771d0f30c13106a26d
Sri <srinu1289@gmail.com>
2026-03-30 18:20:53 +0530

    logs

---
+++ logs.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+USERID=$(id -u)
+LOGS_FOlDER="/var/log/shell-script"
+if [ $USERID -ne 0 ]; then
+  echo "Please run the script as root or with sudo."
+  exit 1
+fi
+
+VALIDATE () {
+  if [ $1 -ne 0 ]; then
+    echo "Failed to install $2."
+ exit 1
+  else
+    echo "$2 installed successfully."
+  fi
+}
+
+dnf install nginx -y
+VALIDATE $? "Installing nginx web server"
+
+
+dnf install mysql -y
+VALIDATE $? "Installing mysql database server"
+
+dnf install nodejs -y
+VALIDATE $? "Installing nodejs runtime"
+
