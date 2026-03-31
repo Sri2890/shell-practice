2f55be45cad71d54e792fb0c6cd6a34b0debdb91
Sri <srinu1289@gmail.com>
2026-03-31 13:06:32 +0530

    CMI

--- S-Robooshop/robooshop.sh
+++ S-Robooshop/robooshop.sh
@@ -12,7 +12,7 @@ aws ec2 run-instances \
     --image-id ami-0220d79f3f480ecf5 \
     --instance-type t3.micro \
     --security-group-ids sg-095bed2c4e0868764 \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
     --query 'Instances[0].PrivateIpAddress' \
     --output text
 
