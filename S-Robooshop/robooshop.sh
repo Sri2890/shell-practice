d500a4487eb6b8655ab12aaf78126e857bb2d750
Sri <srinu1289@gmail.com>
2026-03-31 19:29:57 +0530

    CMI

--- S-Robooshop/robooshop.sh
+++ S-Robooshop/robooshop.sh
@@ -2,6 +2,8 @@
 
 AMI="ami-0220d79f3f480ecf5"
 SGID="sg-095bed2c4e0868764"
+Hosted_zone_id="Z03848281H6OUIMZOLZ40"
+Domain_name="srinivas2890.online"
 
 
 for instance in $@ # mogodb nodejs shell frontend
@@ -25,6 +27,7 @@ do
               --query "Reservations[0].Instances[0].PublicIpAddress" \
               --output text
         )
+        record_name="$Domain_name" # srinivas2890.online
     else     
         IP=$(
             aws ec2 describe-instances \
@@ -32,6 +35,29 @@ do
               --query "Reservations[0].Instances[0].PrivateIpAddress" \
               --output text
         )
+        record_name="$instance.$Domain_name" # mogodb.srinivas2890.online
     fi
+    aws route53 change-resource-record-sets \
+  --hosted-zone-id $Hosted_zone_id \
+  --change-batch '
+  {
+    "Comment": "Adding record for '$instance'",
+    "Changes": [
+      {
+        "Action": "UPSERT",
+        "ResourceRecordSet": {
+          "Name": "'$record_name' ,
+          "Type": "A",
+          "TTL": 1
+          "ResourceRecords": [
+            {
+              "Value": "'$IP'"
+            }
+          ]
+        }
+      }
+    ]
+  }'
  echo "Launched EC2 instance for $instance with IP: $IP"
+ echo "Created DNS record for $instance: $record_name -> $IP"
 done
