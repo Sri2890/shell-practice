a0e13fa31fd002b67afe3649665cc060cdf7a32a
Sri <srinu1289@gmail.com>
2026-03-31 14:38:11 +0530

    CMI

--- S-Robooshop/robooshop.sh
+++ S-Robooshop/robooshop.sh
@@ -4,16 +4,32 @@ AMI="ami-0220d79f3f480ecf5"
 SGID="sg-095bed2c4e0868764"
 
 
-for instance in $@ # mogodb nodejs shell
+for instance in $@ # mogodb nodejs shell frontend
 do
   echo "Launching EC2 instance for $instance..."
   
-aws ec2 run-instances \
-    --image-id ami-0220d79f3f480ecf5 \
+  instance_id=$(aws ec2 run-instances \
+    --image-id $AMI \
     --instance-type t3.micro \
-    --security-group-ids sg-095bed2c4e0868764 \
+    --security-group-ids $SGID \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
     --query 'Instances[0].PrivateIpAddress' \
-    --output text
+    --output text)
+    
+    if [instance_id == "frontend"]; then
+        IP=$(
+            aws ec2 describe-instances \
+              --instance-ids $instance_id \
+              --query 'Reservations[0].Instances[0].PublicIpAddress' \
+              --output text
+        )
+    else     
+        IP=$(
+            aws ec2 describe-instances \
+              --instance-ids $instance_id \
+              --query 'Reservations[0].Instances[0].PrivateIpAddress' \
+              --output text
+        )
+    fi
 
 done
